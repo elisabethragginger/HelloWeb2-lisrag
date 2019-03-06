f05bea01a5b304da0e4c92454fd6911927759798
elisabethragginger <elisabeth.ragginger@htl-salzburg.ac.at>
2019-03-06 16:40:24 +0100

    Firstcommit

---
+++ Assets/Scripts/HelloWebScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWebScript : MonoBehaviour {
+
+	 string helloworld = "HelloWeb";
+    
+    public void PrintHelloWorld()
+    {
+        Debug.Log(helloworld);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/LislsSwitcha.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LislsSwitcha : MonoBehaviour
+{
+    public void LoadScene1()
+    {
+        SceneManager.LoadScene("HelloWeb");
+    }
+
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene("HelloWeb2");
+    }
+}
